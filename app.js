946a777e0201fa80fae58dc75040ef4989695c7e
bartivaldezs <bartivaldez@gmail.com>
2021-08-11 16:18:16 -0400

    helmet and compression added

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+
+
 var createError = require('http-errors');
 var express = require('express');
+var helmet= require('helmet');
+var compression = require('compression');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -8,7 +12,8 @@ var indexRouter = require('./routes/index');
 
 
 var app = express();
-
+app.use(helmet());
+app.use(compression());
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
